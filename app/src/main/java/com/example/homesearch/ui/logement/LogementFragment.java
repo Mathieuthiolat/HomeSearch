644da7d8e1bf134109e33fdbf028c630cee224a5
Mathieu <mathieu.thiolat@gmail.com>
2021-03-11 10:47:35 +0100

    Change preBuild name

--- app/src/main/java/com/example/homesearch/ui/logement/LogementFragment.java
+++ app/src/main/java/com/example/homesearch/ui/logement/LogementFragment.java
@@ -1,4 +1,4 @@
-package com.example.homesearch.ui.dashboard;
+package com.example.homesearch.ui.logement;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.homesearch.R;
 
-public class DashboardFragment extends Fragment {
+public class LogementFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    private LogementViewModel logementViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        logementViewModel =
+                new ViewModelProvider(this).get(LogementViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_logement, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        logementViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/example/homesearch/ui/logement/LogementViewModel.java
+++ app/src/main/java/com/example/homesearch/ui/logement/LogementViewModel.java
@@ -1,14 +1,14 @@
-package com.example.homesearch.ui.dashboard;
+package com.example.homesearch.ui.logement;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class LogementViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public LogementViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
